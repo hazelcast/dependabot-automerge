7051303e1637ad458a6522043be17d6a1c2b54eb
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2024-01-12 15:53:22 +0100

    Fix logging

--- .github/workflows/dependabot-automerge.sh
+++ .github/workflows/dependabot-automerge.sh
@@ -24,7 +24,6 @@ for dependency in "${whitelisted_dependencies[@]}"; do
     gh pr comment "$PR_URL" -b "$MSG"
     echo "$MSG"
     exit 0;
-  else
-    echo "Not a whitelisted patch update of $DEPENDENCY_NAMES. Skipping"
   fi
+  echo "Not a whitelisted patch update of $DEPENDENCY_NAMES. Skipping"
 done
