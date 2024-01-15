3913f6ed2a7035cafe50e7eb84ed5f700087e62d
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2024-01-15 14:50:52 +0100

    Add option to automerge custom update types

--- .github/workflows/dependabot-automerge.sh
+++ .github/workflows/dependabot-automerge.sh
@@ -5,7 +5,7 @@ export GH_DEBUG=${RUNNER_DEBUG:+1}
 
 PR_URL=$1
 DEPENDENCY_NAMES=$2
-UPDATE_TYPE=$3
+UPDATE_TYPE=${3//"version-update:semver-"/}
 
 conf_file="dependabot-automerge-whitelist.conf"
 conf_file_path="repository/.github/workflows/${conf_file}"
@@ -15,18 +15,34 @@ if [ ! -f "$conf_file_path" ]; then
    curl -sSf "$default_conf_file_url" > "$conf_file_path"
 fi
 
-readarray -t whitelisted_dependencies < "$conf_file_path"
+UPDATE_TYPES="major minor patch"
 
-for dependency in "${whitelisted_dependencies[@]}"; do
-  if [[ $DEPENDENCY_NAMES == *"$dependency"* && "$UPDATE_TYPE" == 'version-update:semver-patch' ]]; then
+filter_supported_update_types() {
+  local maximum_update_type=$1
+  local ignored_update_types=${UPDATE_TYPES%%$maximum_update_type*}
+  local position=${#ignored_update_types}
+  echo "${UPDATE_TYPES:$position}"
+}
+
+readarray -t whitelisted_dependencies <<<$(grep "\S" "$conf_file_path")
+
+for dependency_entry in "${whitelisted_dependencies[@]}"; do
+  read -r dependency_name dependency_update_type <<< "$dependency_entry"
+  dependency_update_type="${dependency_update_type:-patch}"
+  if [[ ! "$dependency_update_type" =~ ^(minor|major|patch)$ ]]; then
+    echo "Error: Invalid update type for ${dependency_name}. It must be not set or be one of: $UPDATE_TYPES."
+    exit 1
+  fi
+  allowed_update_types=$(filter_supported_update_types "$dependency_update_type")
+  if [[ $DEPENDENCY_NAMES == *"$dependency_name"* && $allowed_update_types = *$UPDATE_TYPE*  ]]; then
     gh pr merge --auto --squash "$PR_URL"
     gh pr review --approve "$PR_URL"
     gh pr edit --add-assignee "@me" "$PR_URL"
-    MSG="Automerging a whitelisted patch update of $DEPENDENCY_NAMES"
+    MSG="Automerging a whitelisted $UPDATE_TYPE update of $DEPENDENCY_NAMES"
     gh pr comment "$PR_URL" -b "$MSG"
     echo "$MSG"
     exit 0;
   fi
 done
 
-echo "Not a whitelisted patch update of $DEPENDENCY_NAMES. Skipping"
+echo "Non-whitelisted $UPDATE_TYPE update of $DEPENDENCY_NAMES. Skipping"
